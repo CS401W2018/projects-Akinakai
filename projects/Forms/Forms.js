fade9923b9e12dcd11752c98f87801409f1450ea
Isaac Akinaka <akinakaisaac@gmail.com>
2024-11-18 21:40:12 -1000

    addd

--- projects/Forms/Forms.js
+++ projects/Forms/Forms.js
@@ -1,36 +1,46 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
+document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    
-    const First = document.getElementById('FirstNameInput').value;
-    const age = document.getElementById ('age').value;
-    const Last = document.getElementById("LastNameInput").value
 
-    if (!FirstNameInput || !LastNameInput){
-        alert("First name and Last name required!")
+    const First = document.getElementById('FirstNameInput').value;
+    const Last = document.getElementById('LastNameInput').value;
+    const age = document.getElementById('age').value;
+
+    // Validation checks
+    if (!First || !Last) {
+        alert("First name and Last name required!");
         return;
     }
-    if (!age || age <18) {
-        alert(" You must be 18 years of age to continue")
+    if (!age || age < 18) {
+        alert("You must be 18 years of age to continue");
         return;
     }
+
+    // Data to send
     const data = {
-        FirstNameInput: First,
-        LastNameInput: Last,
+        FirstName: First,
+        LastName: Last,
         age: age
-    }
+    };
+
+    // Create XHR object
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "submit.json", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charsset=UTF-8");
-    xhr.onreadystatechange = function (){
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse (xhr.responseText)
-            document.getElementById("message").innerHTML = response.message;
-            document.getElementById("myForm").innerHTML = "";
-        }else if (xhr.readyState === 4){
-            alert('Error submitting form.')
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    // Handle response
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                document.getElementById("message").innerHTML = response.message;
+                document.getElementById("myForm").innerHTML = "";
+            } else {
+                alert('Error submitting form.');
+            }
         }
     };
-    xhr.send (JSON.stringify(data));
-    console.log(data);
 
+    // Send request
+    xhr.send(JSON.stringify(data));
+    console.log(data);
 });
